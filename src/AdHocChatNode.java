48ddd876d104e77b75dc26e5b10d994d0a387af4
nicho <nicholascz2019@outlook.com>
2023-08-04 14:39:06 +0530

    git commit -am "Complete Code" git push

--- src/AdHocChatNode.java
+++ src/AdHocChatNode.java
@@ -8,19 +8,35 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 
-
+/**
+ * The AdHocChatNode class represents a node in the ad hoc chat network.
+ * It provides functionality to send and receive encrypted messages between nodes using AES encryption.
+ */
 public class AdHocChatNode {
     private String username;
     private int port;
     private ServerSocket serverSocket;
     private SecretKeySpec secretKey;
 
+    /**
+     * Constructor to create an instance of AdHocChatNode.
+     *
+     * @param username   The username of the node.
+     * @param port       The port number on which the node will listen for incoming messages.
+     * @param passphrase The passphrase used to generate the AES encryption key.
+     */
     public AdHocChatNode(String username, int port, String passphrase) {
         this.username = username;
         this.port = port;
         this.secretKey = generateSecretKey(passphrase);
     }
 
+    /**
+     * Generates a secret AES encryption key from the provided passphrase
+     * using SHA-256 hashing algorithm.
+     * @param passphrase The passphrase to be used for key generation.
+     * @return The SecretKeySpec object representing the AES encryption key.
+     */
     private SecretKeySpec generateSecretKey(String passphrase) {
         try {
             MessageDigest sha = MessageDigest.getInstance("SHA-256");
@@ -33,12 +49,25 @@ public class AdHocChatNode {
         return null;
     }
 
+    /**
+     * Truncates the given key to the specified length.
+     *
+     * @param key    The key to be truncated.
+     * @param length The length to which the key should be truncated.
+     * @return The truncated key as a byte array.
+     */
     private byte[] truncateKey(byte[] key, int length) {
         byte[] truncatedKey = new byte[length];
         System.arraycopy(key, 0, truncatedKey, 0, length);
         return truncatedKey;
     }
 
+    /**
+     * Encrypts the given message using the AES encryption algorithm with the
+     * generated secret key.
+     * @param message The message to be encrypted.
+     * @return The encrypted message as a Base64 encoded string.
+     */
     private String encryptMessage(String message) {
         try {
             Cipher cipher = Cipher.getInstance("AES");
@@ -51,6 +80,13 @@ public class AdHocChatNode {
         return null;
     }
 
+    /**
+     * Decrypts the given encrypted message using the AES encryption algorithm
+     * with the generated secret key.
+     * @param encryptedMessage The encrypted message
+     *                         to be decrypted (Base64 encoded string).
+     * @return The decrypted message as a plain text string.
+     */
     private String decryptMessage(String encryptedMessage) {
         try {
             Cipher cipher = Cipher.getInstance("AES");
@@ -64,11 +100,20 @@ public class AdHocChatNode {
         return null;
     }
 
+    /**
+     * Starts the node by initializing the server socket to listen for incoming connections on the specified port.
+     * It creates a new thread to handle incoming messages.
+     *
+     * @throws IOException If an I/O error occurs while starting the server socket.
+     */
     public void start() throws IOException {
         serverSocket = new ServerSocket(port);
         new Thread(() -> acceptIncomingMessages()).start();
     }
 
+    /**
+     * Accepts incoming connections from other nodes and creates a new thread to handle each incoming message.
+     */
     private void acceptIncomingMessages() {
         try {
             while (true) {
@@ -80,6 +125,12 @@ public class AdHocChatNode {
         }
     }
 
+    /**
+     * Handles incoming messages from other nodes.
+     * It reads the encrypted message from the input stream of the socket, decrypts it, and prints the decrypted message to the console.
+     *
+     * @param socket The socket representing the incoming connection.
+     */
     private void handleIncomingMessage(Socket socket) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
             String message;
@@ -94,11 +145,22 @@ public class AdHocChatNode {
         }
     }
 
-    public void sendMessage(List<Integer> destPorts, String message) {
+    /**
+     * Sends the given message to multiple destination addresses and ports.
+     * It encrypts the message using the AES encryption algorithm with the generated secret key
+     * and sends the encrypted message to each destination using a separate socket connection.
+     *
+     * @param destAddresses The list of destination IP addresses.
+     * @param destPorts     The list of destination port numbers.
+     * @param message       The message to be sent.
+     */
+    public void sendMessage(List<String> destAddresses, List<Integer> destPorts, String message) {
         String encryptedMessage = encryptMessage(username + ": " + message);
         if (encryptedMessage != null) {
-            for (int port : destPorts) {
-                try (Socket socket = new Socket("localhost", port);
+            for (int i = 0; i < destAddresses.size(); i++) {
+                String ipAddress = destAddresses.get(i);
+                int port = destPorts.get(i);
+                try (Socket socket = new Socket(ipAddress, port);
                      PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
                     writer.println(encryptedMessage);
                 } catch (IOException e) {
@@ -108,6 +170,15 @@ public class AdHocChatNode {
         }
     }
 
+    /**
+     * The main method to run the AdHocChatNode application.
+     * It prompts the user to enter the username, port number, and passphrase for the node.
+     * Then, it creates a new AdHocChatNode instance with the provided information and starts the node.
+     * It also allows the user to send messages to other nodes by entering destination IP addresses and port numbers.
+     *
+     * @param args Command-line arguments (not used in this application).
+     * @throws IOException If an I/O error occurs during user input or starting the node.
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter your username: ");
@@ -120,6 +191,7 @@ public class AdHocChatNode {
         AdHocChatNode node = new AdHocChatNode(username, port, passphrase);
         node.start();
 
+        List<String> destAddresses = new ArrayList<>();
         List<Integer> destPorts = new ArrayList<>();
         while (true) {
             System.out.print("> ");
@@ -127,13 +199,19 @@ public class AdHocChatNode {
             if (message.equalsIgnoreCase("exit")) {
                 break;
             }
+            System.out.print("Enter destination IP addresses (comma-separated): ");
+            String[] ipAddresses = bufferedReader.readLine().split(",");
             System.out.print("Enter destination port numbers (comma-separated): ");
             String[] portNumbers = bufferedReader.readLine().split(",");
+            destAddresses.clear();
             destPorts.clear();
+            for (String ipAddress : ipAddresses) {
+                destAddresses.add(ipAddress.trim());
+            }
             for (String portStr : portNumbers) {
                 destPorts.add(Integer.parseInt(portStr.trim()));
             }
-            node.sendMessage(destPorts, message);
+            node.sendMessage(destAddresses, destPorts, message);
         }
     }
 }
